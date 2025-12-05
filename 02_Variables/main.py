c705de7b76ec99a32bcbcf3d1bab249e7702d811
Your GitHub Username <YourEmail@domain.com>
2025-12-04 21:26:05 -0800

    Assign Multiple Values/main.py

--- 02_Variables/main.py
+++ 02_Variables/main.py
@@ -10,32 +10,28 @@ student=True
 grade='A'
 
 # print() function is used to display a value
-print(name)
-print(Age)
-print(height)
-print(student)
-print(grade)
- 
+#
 # now name has value "john"
 name="john"
+
 print(name)
 
 language="python"
+
 # type() is used to find the data typeof any variable
+
 print(language,type(language))       
 
 y=3.7
+
 print(type(y))
 
 # string can be defined in single,double and triple quotes
+
 name='Anaya'
+
 num="Anaya"
+
 nm='''Anaya'''
+
 print=(name,num,nm)
-
-# we can assign multiple valules to multiple variables in one line
-#a,b,c="raima","swera","saria"
-
-# also can print multiple variables using print function seprated b commas
-#print(a,b,c)
-a, b, c = "raima", "swera", "saria"
